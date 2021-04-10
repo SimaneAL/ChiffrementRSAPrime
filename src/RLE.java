62394bb36aefe6cdc15cb7a53ed6a05d357d4eae
SimaneAL <enamsiy9@gmail.com>
2021-04-10 20:18:19 +0200

    RLE

---
+++ src/RLE.java
@@ -0,0 +1,16 @@
+public class RLE {
+    //compression
+    public void rle(String text) {
+        int n = text.length();
+        for(int i = 0; i < n; i++) {
+            int cpt = 1;
+            while(i < n - 1 && text.charAt(i) == text.charAt(i + 1) ) {
+                cpt++;
+                i++;
+            }
+            System.out.print(text.charAt(i));
+            System.out.print(cpt);
+        }
+
+    }
+}
